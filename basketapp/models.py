8e8a8d79e0c186cca7c65186853c08d103c24cd7
NikolaySibekin <nik-sibekin@yandex.ru>
2019-08-25 21:31:56 +0300

    dz_6

--- basketapp/models.py
+++ basketapp/models.py
@@ -2,6 +2,23 @@ from django.db import models
 from django.conf import settings
 from mainapp.models import Product
 
+@property
+def product_cost(self):
+    return self.product.price * self.quantity
+
+@property
+def total_quantity(self):
+    _items = Basket.objects.filter(user=self.user)
+    _totalquantity = sum(list(map(lambda x: x.quantity, _items)))
+    return _totalquantity
+
+@property
+def total_cost(self):
+    _items = Basket.objects.filter(user=self.user)
+    _totalcost = sum(list(map(lambda x: x.product_cost, _items)))
+    return _totalcost
+
+
 class Basket(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='basket')
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
